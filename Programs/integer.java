2332f04a55b5d4dd7922798a9da61a3029906c46
Coder-sam-007 <padamzain178@gmail.com>
2022-11-04 10:22:36 +0530

    programs added

---
+++ Programs/integer.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class integer {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        System.out.println("You have entered: " + num);
+
+    }
+}
---
+++ Programs/multiply_float.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class multiply_float {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter two floating numbers: ");
+
+        float num1 = in.nextFloat();
+        float num2 = in.nextFloat();
+
+        System.out.println("The product of your Entered numbers are: " + (num1 * num2));
+
+    }
+
+}
