3dfd1c73fb95429f1e025dfc62741aa569c08f60
Coder-sam-007 <padamzain178@gmail.com>
2022-11-04 12:20:33 +0530

    Prime checker modified

--- DMS/PrimeChecker.cpp
+++ DMS/PrimeChecker.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 using namespace std;
-int isPrime(int num){
-if(num<=1)
+
+int isPrime(int num)
 {
-    return 0;
-}
-for(int i=2;i<num;i++)
-{
-    if(num%i==0)
-    return 0;
+    if (num <= 0)
+        return 0;
+
+    for (int i = 2; i < num / 2; i++)
+    {
+        if (num % i == 0)
+            return 0;
+    }
 }
 
-}
 int main()
 {
     int num;
     cout << "Enter a number: ";
     cin >> num;
 
-    if(isPrime(num))
-    {
-        cout<<"PRIME";
-    }
+    if (isPrime(num))
+        cout << num << " is a Prime number." << endl;
+
     else
-    {
-        cout<<"COMPOSITE";
-    }
+        cout << num << " is not a Prime number." << endl;
+
     return 0;
 }
