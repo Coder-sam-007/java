00b166a8a58f1dab24b3e7cdb28311ec4dc03397
Coder-sam-007 <padamzain178@gmail.com>
2022-11-09 10:36:03 +0530

    update

--- Basics/maths.java
+++ Basics/maths.java
@@ -3,14 +3,17 @@ public class maths {
     int num1 = 12;
     double num2 = 12D;
     float num3 = 12.00F;
-    System.out.println(Math.max(num3, num1));
+
+    System.out.println("double num2: " + num2);
+    System.out.println("maximum between num3(12.00F) and num1(12) is: " + Math.max(num3, num1));
 
     double num4 = 123, num5 = 344444;
-    System.out.println(Math.min(num4, num5));
+    System.out.println("minimum value between 123 and 344444 is: " + Math.min(num4, num5));
 
     // generating random nums;
     // math.random() produces random number between 0.0 to 1.0
 
+    System.out.println("Generating random stuff: ");
     System.out.println(Math.random());
     System.out.println(Math.random() * 100);
     System.out.println((int) Math.ceil(Math.random() * 100));
--- Programs/vowel_counter.java
+++ Programs/vowel_counter.java
@@ -1,6 +1,19 @@
 public class vowel_counter {
   public static void main(String[] args) {
-    char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-    
+    char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', };
+    String sentence = "Time is the god of infinity";
+
+    System.out.println(sentence);
+
+    int count = 0;
+
+    for (int i = 0; i < vowels.length; i++) {
+      if (sentence.contains("i")) {
+        count++;
+      }
+    }
+
+    System.out.println("The no. of i present in the above sentece is: " + count);
+
   }
 }
