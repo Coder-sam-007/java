ffeed0efa944d332e47a0ecf482144d208e00b07
coder-sam-007 <sameerjain.coder@gmail.com>
2023-01-13 11:04:53 +0530

    refr

---
+++ Basics/reference_variable.java
@@ -0,0 +1,14 @@
+/* When we create an object (instance) of class then space is reserved in heap memory.
+  
+Now, The space in the heap Memory is created but the question is how to access that space?.  
+
+Then, We create a Pointing element or simply called Reference variable which simply points out the Object(the created space in a Heap Memory).
+
+
+ */
+
+public class reference_variable {
+  public static void main(String[] args) {
+
+  }
+}
